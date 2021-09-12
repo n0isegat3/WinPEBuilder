3023981c6ed1ffbb07a5ffb994fa942e481ab0fa
n0isegat3 <jan@cyber-rangers.com>
2021-09-12 18:27:42 +0200

    updates

--- Add-Scripts/startnet.cmd
+++ Add-Scripts/startnet.cmd
@@ -1,6 +1,5 @@
 @Echo OFF
 wpeinit
-REM: Edit for Carlos Martinez - Date: 11-20-2019 - GitHub @cmartinezone 
 REM You can call your script below
 REM Examples Calling PowerShell: powershell -executionpolicy Unrestricted -noexit -file ".\scriptname.ps1"
 REM Calling VBSCript:            cscript scriptname.vbs
